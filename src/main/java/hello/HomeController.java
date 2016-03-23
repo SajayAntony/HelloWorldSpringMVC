b116e25db7c73555aa26c960431f769b6defe249
Sajay Antony <sajaya@microsoft.com>
2016-03-23 15:24:24 -0700

    Removed parameters.

---
+++ src/main/java/hello/HomeController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {        
+        return "index";
+    }
+}
